f836774ca658a6bc4ff4682563d569532fd0196f
moonandstar0 <norbert.melicharik@gmail.com>
2024-06-12 13:43:25 +0200

    I've added the MVVM basic stuff and some practice code, I'll leave the practice code in for now to get as a reference

--- ProductiviesApp/MauiProgram.cs
+++ ProductiviesApp/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using ProductiviesApp.Models;
+using ProductiviesApp.ViewModels;
 
 namespace ProductiviesApp
 {
@@ -13,7 +15,11 @@ namespace ProductiviesApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                })
+                .RegisterAppServices()
+                .RegisterViews()
+                .RegisterViewModels()
+                .RegisterModels();
 
 #if DEBUG
     		builder.Logging.AddDebug();
@@ -21,5 +27,35 @@ namespace ProductiviesApp
 
             return builder.Build();
         }
+
+        private static MauiAppBuilder RegisterAppServices(this MauiAppBuilder mauiAppBuilder)
+        {
+            _ = mauiAppBuilder.Services.AddSingleton<AppShell>();
+
+            return mauiAppBuilder;
+        }
+
+        private static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+        {
+            _ = mauiAppBuilder.Services.AddTransient<MainPageViewModel>();
+
+            return mauiAppBuilder;
+        }
+
+        private static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+        {
+            _ = mauiAppBuilder.Services.AddTransient<MainPage>();
+
+            return mauiAppBuilder;
+        }
+
+        private static MauiAppBuilder RegisterModels(this MauiAppBuilder mauiAppBuilder)
+        {
+            _ = mauiAppBuilder.Services.AddTransient<Item>();
+
+            return mauiAppBuilder;
+        }
     }
+
+
 }
