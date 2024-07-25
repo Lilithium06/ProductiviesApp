61f90b9e88338a61c48c0cc546c0650c34652d3c
Derek Gooding <djgooding@live.com>
2024-07-25 11:17:14 -0400

    Cleanup null warnings

--- ProductiviesApp.DataAccess/Entities/QuestEntity.cs
+++ ProductiviesApp.DataAccess/Entities/QuestEntity.cs
@@ -6,12 +6,15 @@ namespace ProductiviesApp.DataAccess.Entities;
 public class QuestEntity
 {
     [PrimaryKey, AutoIncrement]
-    public Guid Id { get; set; }
+    public required Guid Id { get; set; }
 
-    public string Name { get; set; }
-    public string Details { get; set; }
-    public string Difficulty { get; set; }
+    [NotNull]
+    public required string Name { get; set; }
+    [NotNull]
+    public required string Details { get; set; }
+    [NotNull]
+    public required string Difficulty { get; set; }
 
     [ManyToMany(typeof(QuestSkillEntity))]
-    public List<SkillEntity> NeededSkills { get; set; }
+    public required List<SkillEntity> NeededSkills { get; set; }
 }
